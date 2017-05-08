6f56faaf60a945b5d09c62916074a2a373dbe1d0
Richard Mackey <dexterm59@gmail.com>
2017-05-07 21:50:06 -0400

    Added file xfer with sample config file, not testedd

--- TransferFiles.py
+++ TransferFiles.py
@@ -1,20 +1,40 @@
 import paramiko
-import os
+import datetime
+from datetime import datetime
+import argparse
+import logging
+import os.path
+import configparser
+
+report_time = datetime.now()
+report_extension = report_time.strftime("%Y%m%d-%H%M%S")
+
+parser = argparse.ArgumentParser(description='Test argument parser with filenames.')
+
+parser.add_argument('config', help='config file for creds, dest, etc')
+
+
+args = parser.parse_args()
+
+config = configparser.ConfigParser()
+config.read(args.config)
+config.sections()
 
 #see if there is a file to transfer
 
 #assign filename to file (may need a loop to xfer multiple files)
 
-server ='server name'
-port = 22
-user = 'user name'
-pw = 'password'
+input_directory = config['natwest']['inputdirectory']
+server = config['natwest']['hostname']
+port = config['DEFAULT']['port']
+user = config['natwest']['username']
+pw = config['natwest']['password']
 
 
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(remote_host, username=user, password=pw)
+client.connect(server, username=user, password=pw)
 
 scp = SCPClient(client.get_transport())
 
